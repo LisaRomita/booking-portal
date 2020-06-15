c316ea255731e454c3b2e94d51f1609792d0049c
LisaRomita <55047778+LisaRomita@users.noreply.github.com>
2020-06-15 11:39:20 +0200

    fix navbar collapse

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
